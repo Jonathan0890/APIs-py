489af2dab238f4b897f9cf115f816ad6e83d17d2
Jonathan0890 <jonathan.coox15@gmail.com>
2025-07-09 17:09:26 -0500

    Ajuste de hotel

--- app.py
+++ app.py
@@ -1,50 +1,8 @@
 from flask import Flask
 from config import app
-from extenciones import bcrypt, jwt, cors
-from routes.product_routes import product_bp
-from routes.category_routes import category_bp
-from routes.product_category_routes import product_category_bp
-from routes.pedido_routes import pedido_bp
-from routes.detalle_pedido_routes import detalle_bp
-from routes.user_routes import user_bp
-from routes.pago_routes import pago_bp
-from routes.resena_routes import resena_bp
-from routes.direccion_routes import direccion_bp
-from routes.carrito_routes import carrito_bp
-from routes.cupones_routes import cupon_bp
-from routes.auth_routes import auth_bp
-from routes.resena_ia_routes import resena_ia_bp
 
 
 # Registrar las rutas
-app.register_blueprint(auth_bp, url_prefix='/api/auth')
-app.register_blueprint(product_bp, url_prefix='/api/products')
-app.register_blueprint(category_bp, url_prefix='/api/categories')
-app.register_blueprint(product_category_bp, url_prefix='/api/products_categories')
-app.register_blueprint(pedido_bp, url_prefix='/api/pedidos')
-app.register_blueprint(detalle_bp, url_prefix='/api/detalles_pedidos')
-app.register_blueprint(user_bp, url_prefix='/api/users')
-app.register_blueprint(pago_bp, url_prefix='/api/pagos')
-app.register_blueprint(resena_bp, url_prefix='/api/resenas')
-app.register_blueprint(direccion_bp, url_prefix='/api/direcciones')
-app.register_blueprint(carrito_bp, url_prefix='/api/carritos')
-app.register_blueprint(cupon_bp, url_prefix='/api/cupones')
-app.register_blueprint(resena_ia_bp, url_prefix='/api/ia/resenas')
-
-
-
-# Inicializa las extensiones con la app
-bcrypt.init_app(app)
-jwt.init_app(app)
-cors.init_app(app, resources={
-    r"/api/*": {
-        "origins": [
-            "http://localhost:3000",
-            "http://127.0.0.1:3000",
-            "https://tusitio.com"
-        ]
-    }
-})
 
 
 @app.route('/')
